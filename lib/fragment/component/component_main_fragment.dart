91ee6d60f3db8e5a4c40cf8637e9e54ecf103fda
mikkel.septiano <112376471+mikkelseptiano23@users.noreply.github.com>
2023-03-06 09:48:48 +0700

    simplify the code

--- lib/fragment/component/component_main_fragment.dart
+++ lib/fragment/component/component_main_fragment.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:sample_flutter/bloc/api/api_bloc.dart';
 import 'package:sample_flutter/bloc/api/api_event.dart';
 import 'package:sample_flutter/bloc/api/api_state.dart';
-import 'package:sample_flutter/constants/colorconstant.dart';
 import 'package:sample_flutter/model/model_data_airline.dart';
-import 'package:sample_flutter/model/model_list_airline.dart';
 
 class MainFragmentContainer extends StatefulWidget {
   const MainFragmentContainer({super.key});
@@ -16,12 +13,13 @@ class MainFragmentContainer extends StatefulWidget {
 }
 
 class _MainFragmentContainerState extends State<MainFragmentContainer> {
-  int selectedPosition = -1;
   final ApiBloc _apiBloc = ApiBloc();
-  static int _page = 0;
-  static const _size = 10;
+  final _size = 10;
 
-  final List<Data>? _dataAirline = [];
+  bool _canLoadMore = false;
+  int _page = 0;
+
+  final List<Data> _dataAirline = [];
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -38,50 +36,46 @@ class _MainFragmentContainerState extends State<MainFragmentContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: BlocProvider(
-        create: (context) => _apiBloc,
-        child: BlocConsumer<ApiBloc, ApiState>(
-            buildWhen: (context, state) {
-              return state is ApiLoaded;
-            },
-            builder: (context, state) {
-              if (state is ApiLoading) {
-                return _buildLoading();
-              } else if (state is ApiLoaded) {
-                if(_dataAirline?.isNotEmpty == true) {
-                  // remove circular progress
-                  _dataAirline?.removeAt((_dataAirline?.length ?? 0) - 1);
-                }
-
-                state.listAirline.data?.asMap().forEach((key, value) {
-                  _dataAirline?.add(value);
-                });
-                _dataAirline?.add(Data()); // for circular item at the end of list
-
-                print("current list: ${_dataAirline?.length}");
-                print("current page: ${_page}");
-
-                return _buildContentView(context, _dataAirline);
-              } else if (state is ApiError) {
-                return _buildEmptyView();
-              } else {
-                return _buildEmptyView();
+    return BlocProvider(
+      create: (context) => _apiBloc,
+      child: BlocConsumer<ApiBloc, ApiState>(
+          buildWhen: (context, state) {
+            return state is ApiLoaded;
+          },
+          builder: (context, state) {
+            if (state is ApiLoading) {
+              return _buildLoading();
+            } else if (state is ApiLoaded) {
+              if(_dataAirline.isNotEmpty == true) {
+                _dataAirline.removeAt(_dataAirline.length - 1); // remove circular progress
               }
-            },
-            listenWhen: (context, state) {
-              return state is ApiError;
-            },
-            listener: (context, state) {
-              if (state is ApiError) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(state.message!),
-                  ),
-                );
-              }
+
+              state.listAirline.data?.asMap().forEach((key, value) {
+                _dataAirline.add(value);
+              });
+              _dataAirline.add(Data()); // for circular item at the end of list
+
+              _canLoadMore = true;
+
+              return _buildContentView(context, _dataAirline);
+            } else if (state is ApiError) {
+              return _buildEmptyView();
+            } else {
+              return _buildEmptyView();
             }
-        ),
+          },
+          listenWhen: (context, state) {
+            return state is ApiError;
+          },
+          listener: (context, state) {
+            if (state is ApiError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message!),
+                ),
+              );
+            }
+          }
       ),
     );
   }
@@ -110,43 +104,42 @@ class _MainFragmentContainerState extends State<MainFragmentContainer> {
               const Duration(seconds: 0), () {
                 setState(() {
                   _page = 0;
-                  _dataAirline?.clear();
+                  _dataAirline.clear();
                   _apiBloc.add(GetAirlineList(page: _page, size: _size));
                 });
               }
           );
         },
-        child: Container(
-          child: ListView.separated(
-            padding: const EdgeInsets.all(10),
-            itemCount: listAirline?.length ?? 0,
-            separatorBuilder: (context, index) {
-              return const Divider();
-            },
-            itemBuilder: (BuildContext context, int index) {
-              if(listAirline![index].sId == null) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                return ListTile(
-                  leading: Image(
-                    width: 50,
-                    image: NetworkImage(listAirline![index].airline![0].logo.toString()),
-                  ),
-                  tileColor: selectedPosition == index ? ColorConstant.blue : null,
-                  title: Text(listAirline[index].name.toString()),
-                  subtitle: Text(listAirline[index].airline![0].name.toString()),
-                );
-              }
-            },
-            controller: _scrollController..addListener(() {
-              if (_scrollController.offset >= _scrollController.position.maxScrollExtent) {
-                setState(() {
-                  _page = _page + 1;
-                  _apiBloc.add(GetAirlineList(page: _page, size: _size));
-                });
-              }
-            }),
-          ),
+        child: ListView.separated(
+          padding: const EdgeInsets.all(10),
+          itemCount: listAirline?.length ?? 0,
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
+          itemBuilder: (BuildContext context, int index) {
+            if(listAirline![index].sId == null) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return ListTile(
+                leading: Image(
+                  width: 50,
+                  image: NetworkImage(listAirline[index].airline![0].logo.toString()),
+                ),
+                tileColor: null,
+                title: Text(listAirline[index].name.toString()),
+                subtitle: Text(listAirline[index].airline![0].name.toString()),
+              );
+            }
+          },
+          controller: _scrollController..addListener(() {
+            if (_scrollController.offset >= _scrollController.position.maxScrollExtent && _canLoadMore) {
+              setState(() {
+                _page = _page + 1;
+                _canLoadMore = false;
+                _apiBloc.add(GetAirlineList(page: _page, size: _size));
+              });
+            }
+          }),
         ),
       ),
     );
